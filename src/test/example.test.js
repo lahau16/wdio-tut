44ec21dbec7c3ac00fe8305b76245257e967c42a
La Thi Hau <h001760@cb.local>
2020-11-26 13:13:26 +0700

    Change a file

--- src/test/example.test.js
+++ src/test/example.test.js
@@ -51,4 +51,24 @@ describe('webdriver.io page', () => {
         // }
     })
 })
+describe('webdriver.io page', () => {
+    it('should have the right title', async () => {
+        // Arrange
+        await browser.url('https://tiki.vn');
+        // Act
+        const INPUT_SEARCH = await browser.$('input[type="text"]');
+        const BTN_SEARCH = await browser.$('//button[contains(text(), "Tìm kiếm")]');
+        await INPUT_SEARCH.setValue("Vong lắc eo");
+        await BTN_SEARCH.click();
+        await browser.pause(3000);
+        // Assert
+        let pageTitle = await browser.getTitle()
+        console.log(`Page title is: ${pageTitle}`);
+        // if (pageTitle.toLowerCase().includes('bỉm em bé')) {
+        //     console.log("Test Passed!!!");
+        // } else {
+        //     console.log("Test Failed!!!");
+        // }
+    })
+})
 
